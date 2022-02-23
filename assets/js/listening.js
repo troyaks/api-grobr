a2f74ad55e9f59f5d35f7dd3551430c5e2cbb2ed
Walisson Melo <walisson.melo.1@gmail.com>
2022-02-23 01:08:43 -0300

    codigo que funciona em um testador de API, mas não no HTML

--- assets/js/listening.js
+++ assets/js/listening.js
@@ -5,10 +5,28 @@ const distribuidor = urlParams.get('distri');
 const tag = urlParams.get('tag');
 
 if (id && distribuidor && tag) {
-    console.log(`deu certo`);
-}
+    console.log(`deu certo Walisson`);
 
-else {
+    let data = {
+        "subject": "Bruna, minha vida teste 2"
+    }
+    const settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.movidesk.com/public/v1/tickets?token=39cbc1bb-ffa6-491e-84f4-eb9687902e5e&id=12007",
+        "method": "PATCH",
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "processData": false,
+        "data": data
+    };
+
+    $.ajax(settings).done(function(response) {
+        console.log(response);
+    });
+
+} else {
     console.log(`faltando algum parametro`);
     console.log(`id : ${id}`);
     console.log(`distribuidor : ${distribuidor}`);
