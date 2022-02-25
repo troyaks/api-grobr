2bd7d298cb669c939da43ac60962f99e6c7fe5f1
troyaks <luannn.cfg@gmail.com>
2022-02-24 23:05:27 -0300

    Some small changes on the syntax

--- assets/js/functions/resolveURL.js
+++ assets/js/functions/resolveURL.js
@@ -1,11 +1,11 @@
 // resolveURL.js
 
 export function resolveParamFromURL () {
-
     const urlParams = new URLSearchParams(location.search); //Getting Parameters array from URL
     const entries = urlParams.entries();
     return entries;
 }
+
 export function paramsToObject(params) {
     const result = {};
         for(const [key, value] of params) { // each 'entry' is a [key, value] tupple
--- assets/js/mainAPI.js
+++ assets/js/mainAPI.js
@@ -1,14 +1,10 @@
-//import { createBodyJSON } from "./functions/create.js";
 import { fetchIt } from "./functions/fetchMethod.js";
 import { findTicketByID } from "./functions/findTicketBy.js";
-import { printOnHTML } from "./functions/print.js";
 import { paramsToObject, resolveParamFromURL } from "./functions/resolveURL.js";
 
 let parameters = resolveParamFromURL(); // Take the parameters from URL and resolve them into variables.
-const bodyOBJ = paramsToObject(parameters); // Turn body request into an object.
-const bodyJSON = JSON.stringify(bodyOBJ); // Turn body object into JSON.
-parameters = resolveParamFromURL(); // Fill the parameters again due to a bug. If we take out this line the code doesn't work.
+const bodyJSON = JSON.stringify(paramsToObject(parameters)); // Turn body request from URL (parameters) into a body object and then turn body object into JSON.
+parameters = resolveParamFromURL(); // Fill the parameters again since it seems to be only "one-time-readable"
 for(const [key, value] of parameters) { window[key] = value; } // Create global variables in "mainAPI.js" based on the entries in the URL.
-const myResource = findTicketByID(id, url); //Get the resource necessary to implement the API methods.
-
-    await fetchIt(myResource, method, bodyJSON); //Fetching method
+const myResource = findTicketByID(id, url); // Get the resource necessary to implement the API methods.
+await fetchIt(myResource, method, bodyJSON); // Applying fetch method
